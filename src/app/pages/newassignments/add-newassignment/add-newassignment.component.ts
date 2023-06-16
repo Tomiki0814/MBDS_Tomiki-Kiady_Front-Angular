92915469071ef1092fbd75540bc45d9199d81d89
Tomiki0814 <tomikiramanantsoa@gmail.com>
2023-06-16 14:30:31 +0300

    update

--- src/app/pages/newassignments/add-newassignment/add-newassignment.component.ts
+++ src/app/pages/newassignments/add-newassignment/add-newassignment.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from "../../../shared/services/api.service";
 
 @Component({
   selector: 'app-add-newassignment',
   templateUrl: './add-newassignment.component.html',
   styleUrls: ['./add-newassignment.component.css']
 })
-export class AddNewassignmentComponent {
+export class AddNewassignmentComponent implements OnInit{
+  urlEtudiant="";
+  urlMatiere="";
 
+  constructor(private apiservice: ApiService) {
+  }
+  ngOnInit(): void {
+    this.getListeEtudiant();
+    this.getListeMatiere();
+  }
+
+  getListeEtudiant(){
+
+  }
+  getListeMatiere(){
+
+  }
 }
