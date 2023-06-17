73a2b4058d0bba96d11ef0066c2f38df739b1715
Tomiki0814 <tomikiramanantsoa@gmail.com>
2023-06-17 06:57:16 +0300

    redirection login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {ApiService} from "../shared/services/api.service";
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  url="login"
+  user={username:"",mdp:""}
+  constructor(private apiservice: ApiService, private router: Router) {
+  }
+
+  Login(){
+    console.log(this.user);
+    this.router.navigate(['']);
+    /*this.apiservice.addEntity(this.url,this.user).subscribe(data =>{
+
+    })*/
+  }
 }
